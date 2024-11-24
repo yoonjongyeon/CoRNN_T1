d847ab62d8565fa250599a0dacedb738d02c6ae6
Jongyeon Yoon <90745298+yoonjongyeon@users.noreply.github.com>
2024-11-24 02:05:16 -0600

    Update pip.sh

--- install/pip.sh
+++ install/pip.sh
@@ -1,2 +1,6 @@
-pip install dipy==1.8.0
+pip install --no-deps dipy==1.8.0
+pip install scipy>=1.9.0
+pip install numpy>=1.23.5
+pip install setuptools>=65.5.0
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+

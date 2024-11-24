30d210d390239b9c65a6b91c4c663c315642ebae
Jongyeon Yoon <90745298+yoonjongyeon@users.noreply.github.com>
2024-11-24 02:21:00 -0600

    Upgrade pip.sh

--- install/pip.sh
+++ install/pip.sh
@@ -1,8 +1,4 @@
 pip install Cython>=0.29.29 h5py>=3.1.0 nibabel>=3.0 packaging>=21 tdm>=4.30.0 trx-python>=0.2.9
+pip install scipy>=1.9.0 numpy>=1.23.5 setuptools>=65.5.0
 pip install --no-deps dipy==1.8.0
-# pip install scipy>=1.9.0
-# pip install numpy>=1.23.5
-# pip install setuptools>=65.5.0
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
-
-
